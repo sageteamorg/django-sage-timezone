8fc67fc6da34c00f00fb1ca66d1922e8b8b0a79a
Sepehr Akbarzadeh <sa.goldeneagle@gmail.com>
2024-10-02 16:27:38 +0330

    feat(translation): add translation capability to the package

--- sage_timezone/forms/timezone.py
+++ sage_timezone/forms/timezone.py
@@ -1,10 +1,12 @@
 import pytz
+
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class TimezoneForm(forms.Form):
     """A form to allow users to select a timezone from a dropdown list."""
 
     timezone = forms.ChoiceField(
-        choices=[(tz, tz) for tz in pytz.all_timezones], label="Select Timezone"
+        choices=[(tz, tz) for tz in pytz.all_timezones], label=_("Select Timezone")
     )
